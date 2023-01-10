adc51b0d3b4c418f587b0d21ac0385f7e5e7b968
Derek Smart <derek@grindaga.com>
2023-01-10 16:35:34 -0500

    skip test if env is not set
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- src/test/groovy/org/liquibase/ext/resource/git/GitPathHandlerIT.groovy
+++ src/test/groovy/org/liquibase/ext/resource/git/GitPathHandlerIT.groovy
@@ -1,21 +1,19 @@
 package org.liquibase.ext.resource.git
 
-import liquibase.plugin.Plugin
 import liquibase.resource.DirectoryResourceAccessor
 import org.apache.commons.io.FileUtils
+import spock.lang.Requires
 import spock.lang.Specification
-import spock.lang.Unroll
 
 class GitPathHandlerIT extends Specification {
+
+    def gitPathHandler = new GitPathHandler()
 
     def cleanup() {
         FileUtils.deleteDirectory(new File(".tmp"));
     }
 
     def "Can Clone Public Repository"() {
-        given:
-        def gitPathHandler = new GitPathHandler()
-
         when:
         def resource = gitPathHandler.getResourceAccessor("https://github.com/liquibase/liquibase-github-action-example.git")
 
@@ -24,10 +22,8 @@ class GitPathHandlerIT extends Specification {
     }
 
 
+    @Requires({ env["LIQUIBASE_GIT_USERNAME"] })
     def "Can Clone Private Repository"() {
-        given:
-        def gitPathHandler = new GitPathHandler()
-
         when:
         def resource = gitPathHandler.getResourceAccessor("https://github.com/liquibase/hashicorp-vault-plugin.git")
 
